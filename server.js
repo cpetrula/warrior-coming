8c185a0f3b9a09698ee01bc10854837f0e37a10e
Chris Petrula <cpetrula@yahoo.com>
2025-08-05 23:25:55 -0700

    moved package.json to root, updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-// server.js
-const express = require('express');
-const path = require('path');
+// // server.js
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
-const PORT = 3000;
+// const app = express();
+// const PORT = 3000;
 
-// Serve static files from the Vue app
-app.use(express.static(path.join(__dirname, 'dist')));
+// // Serve static files from the Vue app
+// app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-// Fallback to index.html for SPA routing
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// // Fallback to index.html for SPA routing
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+import express from 'express'
+import path from 'path'
+
+const app = express()
+
+// // Serve static files from the Vue app
+app.use(express.static('frontend/dist'));
+
+app.listen(3000)
