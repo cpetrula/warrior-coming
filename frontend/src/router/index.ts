d299e7db53622ee1c9f051f40167d35278f72275
Chris Petrula <cpetrula@yahoo.com>
2025-12-03 19:41:00 -0800

    added dynamic conical tags, added redirect from www to non www

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,17 +11,17 @@ import PrivacyPolicy from '../views/PrivacyPolicy.vue';
 import TermsOfService from '../views/TermsOfService.vue';
 
 const routes = [
-  { path: '/', component: Home, meta:{title: 'Home - Warrior Coming'} },
-  { path: '/sermons', component: Sermons, meta:{title: 'Sermons - Warrior Coming'} },
-  { path: '/sermons/:id', component: Sermons, props: true, meta:{title: 'Sermons - Warrior Coming'} },
-  { path: '/blogs', component: Blogs },
-  { path: '/music', component: Music, meta:{title: 'Music - Warrior Coming'} },
-  { path: '/shop', component: Shop },
-  { path: '/admin', component: Admin },
-  { path: '/about', component: AboutUs, meta:{title: 'About Us - Warrior Coming'} },
-  { path: '/contact', component: ContactUs, meta:{title: 'Contact Us - Warrior Coming'} },
-  { path: '/privacy', component: PrivacyPolicy, meta:{title: 'Privacy Policy - Warrior Coming'} },
-  { path: '/terms', component: TermsOfService, meta:{title: 'Terms Of Service - Warrior Coming'} },
+  { path: '/', component: Home, meta: { title: 'Home - Warrior Coming', canonical: 'https://warriorcoming.com/' } },
+  { path: '/sermons', component: Sermons, meta: { title: 'Sermons - Warrior Coming', canonical: 'https://warriorcoming.com/sermons' } },
+  { path: '/sermons/:id', component: Sermons, props: true, meta: { title: 'Sermons - Warrior Coming', canonical: 'https://warriorcoming.com/sermons' } },
+  { path: '/blogs', component: Blogs, meta: { canonical: 'https://warriorcoming.com/blogs' } },
+  { path: '/music', component: Music, meta: { title: 'Music - Warrior Coming', canonical: 'https://warriorcoming.com/music' } },
+  { path: '/shop', component: Shop, meta: { canonical: 'https://warriorcoming.com/shop' } },
+  { path: '/admin', component: Admin, meta: { canonical: 'https://warriorcoming.com/admin' } },
+  { path: '/about', component: AboutUs, meta: { title: 'About Us - Warrior Coming', canonical: 'https://warriorcoming.com/about' } },
+  { path: '/contact', component: ContactUs, meta: { title: 'Contact Us - Warrior Coming', canonical: 'https://warriorcoming.com/contact' } },
+  { path: '/privacy', component: PrivacyPolicy, meta: { title: 'Privacy Policy - Warrior Coming', canonical: 'https://warriorcoming.com/privacy' } },
+  { path: '/terms', component: TermsOfService, meta: { title: 'Terms Of Service - Warrior Coming', canonical: 'https://warriorcoming.com/terms' } },
 ];
 
 const router = createRouter({
@@ -30,8 +30,27 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title || 'Warrior Coming'; // Fallback title
-  next();
+ 
+  if (window.location.hostname === 'www.warriorcoming.com') {
+    window.location.replace(`https://warriorcoming.com${to.fullPath}`);
+  } else {
+      // Update the document title
+    document.title = to.meta.title || 'Warrior Coming'; // Fallback title
+
+    // Update the canonical tag
+    const canonicalUrl = to.meta.canonical || `https://warriorcoming.com${to.path}`;
+    let linkTag = document.querySelector("link[rel='canonical']");
+
+    if (!linkTag) {
+      linkTag = document.createElement('link');
+      linkTag.setAttribute('rel', 'canonical');
+      document.head.appendChild(linkTag);
+    }
+
+    linkTag.setAttribute('href', canonicalUrl);
+
+    next();
+  }
 });
 
 export default router;
