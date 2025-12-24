a83030e42b1a58024aa73d81d173a79cec4aa302
Chris Petrula <cpetrula@yahoo.com>
2025-12-24 13:36:55 -0800

    only add title via route if exists in meta prop

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -15,10 +15,10 @@ import NotFound from '../views/NotFound.vue';
 const routes = [
   { path: '/', component: Home, meta: { title: 'Online Christian Ministry For Faith & Biblical Teaching | Warrior Coming', description: 'Online Christian ministry focused on faith and biblical teaching, offering spiritual encouragement grounded in Scripture. Your spiritual journey awaits.', canonical: 'https://warriorcoming.com/' } },
   { path: '/sermons', component: Sermons, meta: { title: 'Bible Based Christian Sermons For Faith & Growth | Warrior Coming', description: 'Bible based Christian sermons that support faith growth, spiritual strength and practical biblical teaching. Your spiritual journey awaits.', canonical: 'https://warriorcoming.com/sermons' } },
-  { path: '/sermons/:id', component: Sermons, props: true, meta: { title: 'Sermons - Warrior Coming', canonical: 'https://warriorcoming.com/sermons' } },
+  { path: '/sermons/:id', component: Sermons, props: true, meta: { canonical: 'https://warriorcoming.com/sermons' } },
   { path: '/blogs', component: Blogs, meta: { canonical: 'https://warriorcoming.com/blogs' } },
   { path: '/music', component: Music, meta: { title: 'Christian Worship Music For Prayer & Reflection | Warrior Coming', description: 'Christian worship music for prayer and reflection, featuring faith based and Bible inspired worship songs. Your spiritual journey awaits.', canonical: 'https://warriorcoming.com/music' } },
-  { path: '/music/:id', component: Music, props: true, meta: { title: 'Music - Warrior Coming', canonical: 'https://warriorcoming.com/music' } },
+  { path: '/music/:id', component: Music, props: true, meta: { canonical: 'https://warriorcoming.com/music' } },
   { path: '/shop', component: Shop, meta: { canonical: 'https://warriorcoming.com/shop' } },
   { path: '/admin', component: Admin, meta: { noindex: true, canonical: 'https://warriorcoming.com/admin' } },
   { path: '/about', component: AboutUs, meta: { title: 'About Us - Warrior Coming', description: 'We are a Christian faith ministry dedicated to biblical teaching, worship and spiritual encouragement. Read more here.', canonical: 'https://warriorcoming.com/about' } },
@@ -40,7 +40,9 @@ router.beforeEach((to, from, next) => {
     window.location.replace(`https://warriorcoming.com${to.fullPath}`);
   } else {
       // Update the document title
-    document.title = to.meta.title || 'Warrior Coming'; // Fallback title
+      if (to.meta.title) {
+        document.title = to.meta.title;
+      }
 
     // Update the canonical tag
     const canonicalUrl = to.meta.canonical || `https://warriorcoming.com${to.path}`;
